519e3358552ac8eb6c40cba27ae4961ff2fd4a33
majharul <majharuljony163@gmail.com>
2023-08-21 18:24:41 +0600

    [Done] student update and delete and set auth gaurd

--- src/app/modules/student/student.routes.ts
+++ src/app/modules/student/student.routes.ts
@@ -2,21 +2,29 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { StudentController } from './student.controller';
 import { StudentValidation } from './student.validations';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
 router.patch(
   '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(StudentValidation.updateStudentZodSchema),
   StudentController.updateStudent
 );
 
 router.post(
   '/create-student',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
   validateRequest(StudentValidation.createStudentZodSchema),
   StudentController.createStudent
 );
-router.delete('/:id', StudentController.deleteStudent);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.SUPER_ADMIN),
+  StudentController.deleteStudent
+);
 router.get('/:id', StudentController.getSingleStudent);
 router.get('/', StudentController.getAllStudents);
 
