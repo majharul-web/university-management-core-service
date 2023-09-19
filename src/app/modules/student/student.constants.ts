0cf94857fc955f1c60fe5889c306c6f2f9476802
majharul <majharuljony163@gmail.com>
2023-09-19 13:06:02 +0600

    Done Faulty part using API gateWay

--- src/app/modules/student/student.constants.ts
+++ src/app/modules/student/student.constants.ts
@@ -32,5 +32,5 @@ export const studentRelationalFieldsMapper: { [key: string]: string } = {
 };
 
 export const EVENT_STUDENT_CREATED = 'student.created';
-export const EVENT_FACULTY_CREATED = 'faculty.created';
+
 export const EVENT_ADMIN_CREATED = 'admin.created';
